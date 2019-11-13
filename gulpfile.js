c4254a452312061e83f1f5c45a67d71bd5ddeda6
Alexander <mrreads@yandex.com>
2019-11-13 18:25:37 +0300

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', () => {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass()) // из scss в css
+        .pipe(autoprefixer()) //autoprefixer
         .pipe(gulp.dest('./public/css/'));
 });
