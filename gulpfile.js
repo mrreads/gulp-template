fc19f513898aa34e66e806818422d5a4b2588e94
Alexander <mrreads@yandex.com>
2019-11-13 20:58:57 +0300

    more files to move

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,29 @@ let gulp = require('gulp'),
     browserSync = require('browser-sync');
 
 gulp.task('moveFiles', () => {
-    return gulp.src('./src/**/*.html')
+    return gulp.src('./src/**/*.{html,php}')
         .pipe(gulp.dest('./public/')),
-    gulp.src('./src/audio/**')
+    gulp.src('./src/audio/*')
         .pipe(gulp.dest('./public/audio/')),
     gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer())
         .pipe(csscomb())
         .pipe(gulp.dest('./public/css/')),
-    gulp.src('./src/fonts/**')
-        .pipe(gulp.dest('./public/fonts/'));
+    gulp.src('./src/fonts/*')
+        .pipe(gulp.dest('./public/fonts/')),
+    gulp.src('./src/php/*')
+        .pipe(gulp.dest('./public/php/')),
+    gulp.src('./src/sql/**/*.sql')
+        .pipe(gulp.dest('./public/sql/'));
 });
 
 gulp.task('html', () => {
-    return gulp.src('./src/**/*.html')
+    return gulp.src('./src/**/*.{html,php}')
         .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('css', () => {
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src('./src/scss/**/*.{scss,sass}')
         .pipe(sass())
         //.pipe(uncss({ html: ['./src/**/*.html'] }))
         .pipe(autoprefixer())
